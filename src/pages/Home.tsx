ca954024e65d070e6733523531cd9db24d472c31
cansoyluu <m.cannsoylu@gmail.com>
2024-02-29 19:24:16 +0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { UsersType } from "../types/user";
+import UserCard from "../companets/UserCard";
+
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+const Home = () => {
+  const [users, setUsers] = useState<UsersType>([]);
+
+  const getUsers = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  });
+
+  return (
+    <div>
+      {users.map((user) => (
+        <UserCard key={user.id} user={user}/>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
